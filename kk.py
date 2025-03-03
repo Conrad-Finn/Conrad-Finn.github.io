582e6ab8f38808cc098eefb8aebbb3e618741be9
Conrad-Finn <shushuijun@gmail.com>
2025-03-03 22:00:57 +0800

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,14 @@
+# 初始化Git仓库
+git init
+
+# 添加所有文件
+git add .
+
+# 提交更改
+git commit -m "初始网站上传"
+
+# 添加GitHub远程仓库
+git remote add origin https://github.com/Conrad-Finn/Conrad-Finn.github.io.git
+
+# 推送到GitHub
+git push -u origin main
